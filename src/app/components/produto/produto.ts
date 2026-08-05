8270ad9e236f328f1b02dc9b972a1e0dd322d2f2
Kassya <kassyabrito2014@gmail.com>
2026-08-05 11:35:08 -0300

    FIM DE AULA _ CONTROLANDO O FLUXO DA MINHA MENTE - 05.08

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
+
+
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [CurrencyPipe, UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
- nome = 'Produto Exemplo';
- preco = 149.99;
- descricao = 'Você é pobre, nem adianta tentar comprar';
 
+  @Input() nome: string = '';
+  @Input() preco: number = 0;
+  @Output() produtoSelecionado = new EventEmitter<string>();
+
+  selecionarProduto() {
+    this.produtoSelecionado.emit(this.nome);
+  }
+
+
+//=================================================
+//              Aprendi no dia 04.08
+//=================================================
+
+
+//  nome = 'Produto Exemplo';
+//  preco = 149.99;
+
+
+
+//=================================================
+//              Aprendi no dia 04.08
+//=================================================
+
+
+
+//=================================================
+//              Aprendi no dia 05.08
+//=================================================
+//  mostrarPreco = true;
+ 
+ 
+ 
+ 
+//  produtos = [
+//   { nome: 'Monitor', preco: 1500 },
+//   { nome: 'Mouse',    preco: 150  },
+//   { nome: 'Teclado',  preco: 250  },
+//   {nome: 'Caixa de Som', preco: 90}
+// ];
+
+
+
+//=================================================
+//              Aprendi no dia 05.08
+//=================================================
 }
+
+
---
+++ src/app/pipes/preco-formatado-pipe.spec.ts
@@ -0,0 +1,9 @@
+import { PrecoFormatadoPipe } from './preco-formatado-pipe';
+
+describe('PrecoFormatadoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrecoFormatadoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
+
