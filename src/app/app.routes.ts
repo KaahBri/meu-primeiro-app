e61e836d5db85f5e3b3d2ac5f614d6cfda697b85
Kassya <kassyabrito2014@gmail.com>
2026-08-11 09:20:54 -0300

    Rotas do comercio - 11.08

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 
   import { Routes } from '@angular/router';
+  import { Home } from './features/home/home/home';
   import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
   import { Carrinho } from './features/carrinho/carrinho/carrinho';
 
+
+  // São as rotas bb
   export const routes: Routes = [
-    {
-      path: '',
-      component: ListaProdutos,
-    },
-    {
-      path: 'carrinho',
-      component: Carrinho,
-    },
+    { path: '', component: Home }, // Cai em Home
+    { path: 'produtos', component: ListaProdutos }, // Cai em produtos
+    { path: 'carrinho', component: Carrinho }, // Cai em Carrinhos
   ];
