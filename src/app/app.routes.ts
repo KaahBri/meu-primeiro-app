6aa0a31fbbd4210e5f7746a1e4c8aeb3e847f687
Kassya <kassyabrito2014@gmail.com>
2026-08-19 08:57:16 -0300

    19.08 - Controle de Acesso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/auth.guard';
+import { adminGuard } from './core/admin.guard';
+
 export const routes: Routes = [
   {
     path: '',
@@ -17,11 +19,26 @@ export const routes: Routes = [
   },
   {
     path: 'checkout',
+    canActivate: [authGuard],
     loadComponent: () => import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
+  },
+    {
+    path: 'admin',
+    canActivate: [adminGuard],
+    loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'acesso-negado',
+    loadComponent: () =>
+      import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado),
   },
   {
     path: '**',
     redirectTo: '', // redireciona para a página inicial
   }
-  
+
 ];
--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,19 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
 
-  import { Component, inject } from '@angular/core';
-  import { MatToolbarModule } from '@angular/material/toolbar';
-  import { MatButtonModule } from '@angular/material/button';
-  import { RouterLink } from '@angular/router';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { AuthService } from '../../../core/services/auth.service';
 
-  import { CarrinhoService } from '../../../core/services/carrinho.service';
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  private carrinhoService = inject(CarrinhoService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-  @Component({
-    selector: 'app-header',
-    imports: [MatToolbarModule, MatButtonModule, RouterLink],
-    templateUrl: './header.html',
-    styleUrl: './header.css',
-  })
-  export class Header {
-    private carrinhoService = inject(CarrinhoService);
-    quantidade = this.carrinhoService.quantidade;
+  quantidade = this.carrinhoService.quantidade;
+  estaLogado = this.authService.estaLogado;
+  usuarioAtual = this.authService.usuarioAtual;
+
+  sair() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
+}
 
